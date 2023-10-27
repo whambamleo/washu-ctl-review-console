1e420861196d59c14eb842cb7cea400afee5acee
whambamleo <89803624+whambamleo@users.noreply.github.com>
2023-10-26 20:49:01 -0500

    qualtrics api handler implemented for getting responses

--- index.php
+++ index.php
@@ -1,24 +1,37 @@
+<?php
+include('qualtrics.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Your Site Title</title>
+    <title> CTL Review Console </title>
     <!-- Meta -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Your Site Description">
     <meta name="author" content="Your Name or Author">
-    <link rel="shortcut icon" href="images/logo.png">
+<!--    <link rel="shortcut icon" href="images/logo.png">-->
 
     <!--    FontAwesome CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <!--    Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <?php wp_head(); ?> <!-- this loads style.css, but I think it also loads other stuff we don't need -->
+    <!-- this loads style.css, but I think it also loads other stuff we don't need -->
+<!--    --><?php //wp_head(); ?><!-- -->
 </head>
 
 <body>
-<p id="test"> testing again </p>
+    <p id="test"> CTL Review Console </p>
+    <?php
+    try {
+        getResponses();
+    } catch (Exception $e) {
+    }
+    echo "<p>";
+    echo "</p>";
+    ?>
 </body>
 </html>
---
+++ qualtrics.php
@@ -0,0 +1,152 @@
+<?php
+
+/* TODO:
+ * Make function for redundant HTTP request setup
+ * Make sure the sleep and request loop in getResponses eventually times out and throws an exception
+*/
+
+$api_config = include('qualtrics_config.php');
+$dataCenterId = $api_config['dataCenterId'];
+$api_token = $api_config['x-api-token'];
+$surveyId = $api_config['surveyId'];
+global $dataCenterId, $surveyId, $api_token;
+
+function getResponses() {
+    try {
+        $progressId = startResponseExport();
+    } catch (Exception $e) {
+        echo 'Failed while trying to initiate export:' .$e->getMessage();
+        return;
+    }
+
+    try {
+        $fileId = checkExportProgress($progressId);
+        while (strlen($fileId) == 0) {
+            sleep(3);
+            $fileId = checkExportProgress($progressId);
+        }
+        echo $fileId;
+    } catch (Exception $e) {
+        echo 'Failed while checking progress of export:' .$e->getMessage();
+        return;
+    }
+
+    try {
+        echo getResponseJSON($fileId);
+    } catch (Exception $e) {
+        echo 'Failed while fetching responses JSON file:' .$e->getMessage();
+        return;
+    }
+}
+
+/**
+ * Initiates a generic response export and returns a progressID for
+ * following calls
+ * @return string progressID
+ * @throws Exception if call fails
+ */
+function startResponseExport(): string
+{
+    global $dataCenterId, $surveyId, $api_token;
+    $url = "https://$dataCenterId.qualtrics.com/API/v3/surveys/$surveyId/export-responses";
+    $headers = [
+        "X-API-TOKEN: $api_token",
+        'Content-Type: application/json',
+    ];
+    $data = [
+        'format' => 'json',
+        'compress' => 'false',
+    ];
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        throw new Exception('Curl error: ' . curl_error($ch));
+    } else {
+        curl_close($ch);
+        $response_as_json = json_decode($response);
+        return $response_as_json->result->progressId;
+    }
+}
+
+/**
+ * Checks the progress of an export and returns the fileId when the export
+ * is complete. If the export is not done, it returns an empty string
+ * @return string fileId
+ * @throws Exception if call fails
+ */
+function checkExportProgress($progressId): string {
+    global $dataCenterId, $surveyId, $api_token;
+
+    if ($progressId == null) {
+        throw new Exception("Progress ID field provided was null!");
+    }
+
+    $fileId = "";
+    $url = "https://$dataCenterId.qualtrics.com/API/v3/surveys/$surveyId/export-responses/$progressId";
+    $headers = [
+        "X-API-TOKEN: $api_token",
+        'Content-Type: application/json',
+    ];
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        throw new Exception('Curl error: ' . curl_error($ch));
+    } else {
+        curl_close($ch);
+        $response_as_json = json_decode($response);
+        if ($response_as_json === null) {
+            throw new Exception('Failed to decode JSON response');
+        }
+        if ($response_as_json->result->status == "failed") {
+            throw new Exception("File export failed!");
+        }
+
+        if ($response_as_json->result->status == "complete" && $response_as_json->result->percentComplete == "100") {
+            $fileId = $response_as_json->result->fileId;
+        }
+
+        return $fileId;
+    }
+}
+
+/**
+ * Uses the fileId to fetch the final JSON of responses
+ * @return mixed responses
+ * @throws Exception if call fails
+ */
+function getResponseJSON($fileId) {
+    global $dataCenterId, $surveyId, $api_token;
+
+    if ($fileId == null) {
+        throw new Exception("File ID field provided was null!");
+    }
+
+    echo $fileId;
+
+    $url = "https://$dataCenterId.qualtrics.com/API/v3/surveys/$surveyId/export-responses/$fileId/file";
+    $headers = [
+        "X-API-TOKEN: $api_token",
+        'Content-Type: application/json',
+    ];
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        throw new Exception('Curl error: ' . curl_error($ch));
+    } else {
+        curl_close($ch);
+        return $response;
+    }
+}
+?>
