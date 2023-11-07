a25060aa063bc01f0e347afd263b7ab8c05e7db9
whambamleo <89803624+whambamleo@users.noreply.github.com>
2023-11-07 13:19:53 -0600

    added popper import for the dropdown menus

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Your Site Description">
     <meta name="author" content="Your Name or Author">
-<!--    <link rel="shortcut icon" href="images/logo.png">-->
+    <!--    <link rel="shortcut icon" href="images/logo.png">-->
 
     <!--    FontAwesome CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
@@ -16,14 +16,16 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!-- this loads style.css, but I think it also loads other stuff we don't need -->
-<!--    --><?php //wp_head(); ?><!-- -->
+    <!--    --><?php //wp_head(); ?><!-- -->
+
 </head>
 
 <body>
-    <?php
-    include get_template_directory().'/templates/dashboard.php';
-    echo '<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>';
-    ?>
+<?php
+    include get_template_directory() . '/templates/dashboard.php';
+    echo '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script></script>';
+?>
 </body>
 </html>
