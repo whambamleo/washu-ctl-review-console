277820621a2190e590aa9d353f8e0c084f18149c
whambamleo <89803624+whambamleo@users.noreply.github.com>
2023-11-06 21:53:58 -0600

    converted response fields to private and wrote getters for response collection

--- classes/Response.php
+++ classes/Response.php
@@ -1,21 +1,38 @@
 <?php
 class Response {
-    public $responseId;
-    public $formStatus;
-    public $formSubmissionDate;
-    public $formQuestionResponses;
+    private $responseId;
+    private $formStatus;
+    private $formSubmissionDate;
+    private $formQuestionResponses;
 
     public function __construct($response) {
         $this->responseId = $response['responseId'];
-        $this->values = $response['values'];
-        $this->formStatus = $this->values['formStatus'];
-        $this->formSubmissionDate = $this->values['endDate'];
+        $values = $response['values'];
+        // Extracting relevant fields
+        $this->formStatus = $values['formStatus'];
+        $this->formSubmissionDate = $values['endDate'];
         
         foreach ($response['values'] as $key => $value) {
             if (substr($key, 0, 3) === "QID") {
                 $this->formQuestionResponses["$key"] = $value;
             }
         }
+    }
+
+    public function getResponseId() {
+        return $this->responseId;
+    }
+
+    public function getFormStatus() {
+        return $this->formStatus;
+    }
+
+    public function getFormSubmissionDate() {
+        return $this->formSubmissionDate;
+    }
+
+    public function getFormQuestionResponses() {
+        return $this->formQuestionResponses;
     }
 }
 ?>
--- classes/ResponseCollection.php
+++ classes/ResponseCollection.php
@@ -21,18 +21,17 @@ class ResponseCollection
     {
         $cards = '<div class="cards">';
         foreach ($this->responses as $response) {
-            $fieldValues = array_values($response->formFieldResponses);
+            $questionResponses = array_values($response->getFormQuestionResponses());
             $card = '<div class="card" style="width: 50rem;">';
             $card = $card . '<div class="card-body">';
-            $card = $card . '<h5 class="card-title">' . reset($fieldValues) . '</h5>';
-            $card = $card . '<p class="card-text">Tool Name: ' . next($response->formFieldResponses) . "</p>";
-            $card = $card . '<p class="card-text text-muted" style="text-align: right;">Submitted on: ' . $response->formSubmissionDate . '</p>';
-            $card = $card . '<p class="card-text text-right position-absolute" style="top: 0; right: 0; border: 1px solid green; border-radius: 10px; padding: 5px; margin: 5px;">' . $response->formStatus . '</p>';
+            $card = $card . '<h5 class="card-title">' . reset($questionResponses) . '</h5>';
+            $card = $card . '<p class="card-text">Tool Name: ' . next($questionResponses) . "</p>";
+            $card = $card . '<p class="card-text text-muted" style="text-align: right;">Submitted on: ' . $response->getFormSubmissionDate() . '</p>';
+            $card = $card . '<p class="card-text text-right position-absolute" style="top: 0; right: 0; border: 1px solid green; border-radius: 10px; padding: 5px; margin: 5px;">' . $response->getFormStatus() . '</p>';
             $card = $card . '</div></div>';
             $cards = $cards . $card;
         }
-        $cards = $cards . "</div>";
-        return $cards;
+        return $cards . "</div>";
     }
 }
 ?>
