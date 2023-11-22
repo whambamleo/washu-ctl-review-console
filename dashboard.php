11799bf7264cf8613afe3603fbb5f70bdfbfc1c3
whambamleo <89803624+whambamleo@users.noreply.github.com>
2023-11-21 20:54:52 -0600

    hooked up dashboard css correctly

--- dashboard.php
+++ dashboard.php
@@ -2,14 +2,7 @@
 /*
 Template Name: Dashboard
 */
-// TODO: Code below not working. I temporarily manually added the CSS from wp-admin but we should figure out how to automate this
-//add_action( 'wp_enqueue_scripts', 'dashboard_style' );
-//
-//function dashboard_style(): void
-//{
-//    wp_register_style( 'dashboard', get_stylesheet_directory_uri() . '/styles/dashboard.css');
-//    wp_enqueue_style( 'dashboard', get_stylesheet_directory_uri() . '/styles/dashboard.css');
-//}
+
 ?>
 
 <script>
@@ -186,9 +179,16 @@ Template Name: Dashboard
         window.location.href = url;
     }
 </script>
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<!--    FontAwesome CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+<!--    Bootstrap CSS TODO: upgrade everything to 5.2 -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<!--    Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" i
+      ntegrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+<?php wp_head(); ?>
 
 <div>
     <!-- Top Banner -->
@@ -263,3 +263,8 @@ Template Name: Dashboard
         </div>
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script></script>
+?>
